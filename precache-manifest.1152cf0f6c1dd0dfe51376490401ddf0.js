d38918e7cf55d7757b9c8c6cbc9bbbe00075c8dd
AnubhavK27 <37549611+AnubhavK27@users.noreply.github.com>
2020-04-12 15:40:06 +0530

    Updates

---
+++ precache-manifest.1152cf0f6c1dd0dfe51376490401ddf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769fce3907cd4a1f72f17b7133d98308",
+    "url": "/FaceApp/index.html"
+  },
+  {
+    "revision": "4c7e00f3ba69e0be1b53",
+    "url": "/FaceApp/static/css/main.234e32b7.chunk.css"
+  },
+  {
+    "revision": "9f76a00d275a7f13af1a",
+    "url": "/FaceApp/static/js/2.83c7a6db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FaceApp/static/js/2.83c7a6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7e00f3ba69e0be1b53",
+    "url": "/FaceApp/static/js/main.2a23f872.chunk.js"
+  },
+  {
+    "revision": "d77349e95182dc4c31a2",
+    "url": "/FaceApp/static/js/runtime-main.fc447320.js"
+  },
+  {
+    "revision": "a608061bcd9a03b3a00fc0417397ece0",
+    "url": "/FaceApp/static/media/Goddess.a608061b.jpg"
+  }
+]);
